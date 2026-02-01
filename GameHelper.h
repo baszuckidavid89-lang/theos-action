c919603f0a9164ee19bfb56cb339947de7f7a0de
baszuckidavid89-lang <baszuckidavid89@gmail.com>
2026-02-02 02:18:13 +0400

    Update GameHelper.h

--- GameHelper.h
+++ GameHelper.h
@@ -1,29 +1,9 @@
-#ifndef GAMEHELPER_H
-#define GAMEHELPER_H
-
 #import <Foundation/Foundation.h>
 
-// Standard Unity Vector3 structure
-#ifndef VECTOR3_DEFINED
-#define VECTOR3_DEFINED
-typedef struct Vector3 {
-    float x;
-    float y;
-    float z;
-} Vector3;
-#endif
+typedef struct Vector3 { float x; float y; float z; } Vector3;
 
 @interface GameHelper : NSObject
-
-@property (nonatomic, assign) BOOL isInitialized;
-@property (nonatomic, assign) void* prefabGeneratorClass;
-@property (nonatomic, assign) void* netPlayerClass;
-
 + (instancetype)sharedHelper;
-- (void)initializeGameClasses;
 - (void)spawnItem:(NSString *)itemName position:(Vector3)pos;
 - (void)giveSelfMoney;
-
 @end
-
-#endif /* GAMEHELPER_H */
